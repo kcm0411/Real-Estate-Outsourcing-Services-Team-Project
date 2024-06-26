9a2f2b68ea474bc19f7e9daa1f05be37db8ecdfc
Changmin Kim <kcm0411@gmail.com>
2024-06-26 21:19:45 +0900

    Fork Test

--- src/main/java/com/sparta/realestatefeed/entity/QnA.java
+++ src/main/java/com/sparta/realestatefeed/entity/QnA.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class QnA extends Timestamped {
 
+    // 11
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "qna_id", nullable = false)
